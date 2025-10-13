03b4d21a2ba5a1466a85ffd2bcec76ffa3e188ae
pooya <poya181@gmail.com>
2025-10-13 12:39:15 +0330

    feat (appController): create AppController.kt

---
+++ app/src/main/java/ir/pooyadev/minidailyfoodloggingapp/AppController.kt
@@ -0,0 +1,10 @@
+package ir.pooyadev.minidailyfoodloggingapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppController() : Application() {
+
+
+}
